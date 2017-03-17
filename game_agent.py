566fca46f297cd9afdb7e071b5e84c0dd9232601
Yevgen Nerush <yevgen.nerush@gmail.com>
2017-03-17 12:04:19 +0100

    Pass the test_heuristic test by implementing any heuristic in custom_score().

--- game_agent.py
+++ game_agent.py
@@ -37,8 +37,16 @@ def custom_score(game, player):
     float
         The heuristic value of the current game state to the specified player.
     """
+    # apply score difference heuristic function
+    if game.is_loser(player):
+        return float("-inf")
 
-    return len(game.get_legal_moves(player))
+    if game.is_winner(player):
+        return float("inf")
+
+    own_moves = len(game.get_legal_moves(player))
+    opp_moves = len(game.get_legal_moves(game.get_opponent(player)))
+    return float(own_moves - opp_moves)
 
 
 class CustomPlayer:
